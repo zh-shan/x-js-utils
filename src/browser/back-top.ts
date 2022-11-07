8f76f2bc8018cd93985dfd66715462c4f34bc980
haishan.zhang <haishan.zhang@southgis.com>
2022-11-07 13:36:57 +0800

    add: 添加 backTop、getTopWindow 函数

---
+++ src/browser/back-top.ts
@@ -0,0 +1,11 @@
+/**
+ * 回到顶部
+ */
+function backTop (): void {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
+export default backTop
---
+++ src/browser/get-top-window.ts
@@ -0,0 +1,26 @@
+/**
+ * 获取最顶层的同源window
+ * @param {Window} win 当成window
+ * @returns {Window}
+ */
+function getTopWindow(win: Window): Window {
+  if (win === win.top) {
+    return win
+  }
+
+  try {
+    // 跨域检测
+    win.parent.location && win.parent.location.origin
+  } catch (e) {
+    return win
+  }
+
+  // 往上递归同源window
+  if (win.location.origin === win.parent.location.origin) {
+    return getTopWindow(win.parent)
+  }
+
+  return win
+}
+
+export default getTopWindow
